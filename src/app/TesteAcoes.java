5158a0c1275d3bfe2684814f2efc1013050e72b7
Gabriel M. Alves <gabriel.marcelino@gmail.com>
2020-09-10 16:12:12 -0300

    iniciar repositório.

---
+++ src/app/TesteAcoes.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package app;
+
+import core.Acao;
+import data.ProcessaCsv;
+
+/**
+ *
+ * @author gabriel
+ */
+public class TesteAcoes {
+    public static void main(String[] args) {
+        ProcessaCsv csv = new ProcessaCsv("/home/gabriel/Downloads/PETR4.SA.csv");
+        Acao petrobras = new Acao("PETR4");
+        petrobras.addSerieCandles(csv.gerarSerieCandles());
+        System.out.println(petrobras.toString());
+    }
+}
---
+++ src/core/TimeFrame.java
@@ -0,0 +1,8 @@
+package core;
+
+public enum TimeFrame {
+    DIARIO,
+    SEMANAL,
+    MENSAL,
+    ANUAL;
+}
