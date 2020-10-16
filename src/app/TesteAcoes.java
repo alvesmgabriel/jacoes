990796931c9463eead7dbb40b788d495b4acb1f1
Gabriel M. Alves <gabriel.marcelino@gmail.com>
2020-10-16 14:43:34 -0300

    criar a interface gráfica principal

--- src/app/TesteAcoes.java
+++ src/app/TesteAcoes.java
@@ -12,6 +12,7 @@ import data.ProcessaCsv;
  *
  * @author gabriel
  */
+// /home/gabriel/Documentos/010-ifsp/00-aula/bcc-lpo3/ativos
 public class TesteAcoes {
     public static void main(String[] args) {
         ProcessaCsv csv = new ProcessaCsv("/home/gabriel/Downloads/PETR4.SA.csv");
